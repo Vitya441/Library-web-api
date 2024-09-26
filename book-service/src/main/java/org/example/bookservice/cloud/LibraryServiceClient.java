3a685766d3ee20ba172568b9b1c18456a0ae38b0
Vitya441 <maksimovviktor554@gmail.com>
2024-09-26 21:41:39 +0300

    preparation for deployment

--- book-service/src/main/java/org/example/bookservice/cloud/LibraryServiceClient.java
+++ book-service/src/main/java/org/example/bookservice/cloud/LibraryServiceClient.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@FeignClient(name = "library-service", url = "http://localhost:8081")
 @FeignClient(name = "library-service")
 public interface LibraryServiceClient {
     @PostMapping("/library/register")
